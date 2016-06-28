de3c67ec944f45633ab8b2fca55a24bbf8066c21
Rafael Foster <rafaelgfoster@gmail.com>
2016-06-28 16:04:33 -0300

    Adding ability to users can choice the config file with an sys argument.

--- main.py
+++ main.py
@@ -1,90 +1,104 @@
 #!/usr/bin/python
 
-
 import os
 import re
 import git
+import sys
 import yaml
 import socket
 import pexpect
 from db import DB
 from fgt import FGT
-from log import LOG	
+from log import LOG
 
-global cfg 
-with open("config.yaml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+def main(configFile):
+    with open(configFile, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
 
-defaultRepo = cfg['repofolder'] 
+    if len(cfg) == 0:
+        print "Could not obtain configs from config file."
+        exit()
 
-FTPUser = cfg['ftp']['FTPUser']
-FTPPass = cfg['ftp']['FTPPass']
-FTPHost = cfg['ftp']['FTPHost']
+    defaultRepo = cfg['repofolder']
+    FTPUser = cfg['ftp']['FTPUser']
+    FTPPass = cfg['ftp']['FTPPass']
+    FTPHost = cfg['ftp']['FTPHost']
 
-DBUser = cfg['database']['DBUser']
-DBPass = cfg['database']['DBPass']
-DBHost = cfg['database']['DBHost']
-DBBase = cfg['database']['DBBase']
+    DBUser = cfg['database']['DBUser']
+    DBPass = cfg['database']['DBPass']
+    DBHost = cfg['database']['DBHost']
+    DBBase = cfg['database']['DBBase']
 
-def main():	
-	fgt = FGT()
-	db = DB()
-	os.chdir(defaultRepo)
-	repo = git.Repo( defaultRepo )
-	for FGTDevice in db.getAll():
-		ID         = FGTDevice[0]
-		STATUS     = FGTDevice[1]
-		NAME	   = FGTDevice[2]
-		SERIAL     = FGTDevice[3]
-		CLIENT     = FGTDevice[4]
-		IP         = FGTDevice[5]
-		IP2        = FGTDevice[6]
-		PORT	   = FGTDevice[7]
-		USER       = FGTDevice[8]
-		PASSWORD   = FGTDevice[9]
-		FREQ_CHECK = FGTDevice[10]
-	
-		IPAddr = fgt.connFGT(IP, IP2, PORT)
-		print IPAddr
-		if IPAddr:
-			outFGTInfo = fgt.runFGTCommand(USER, PASSWORD, IPAddr, PORT, "get system status")
-			if "FortiOS" in outFGTInfo:
-				FGTInfo = fgt.getFGTInfo(outFGTInfo)
-				FGTName   = FGTInfo[0]
-				FGTSerial = FGTInfo[1]
+    fgt = FGT()
+    db = DB(cfg['database'])
+    os.chdir(defaultRepo)
+    repo = git.Repo( defaultRepo )
+    for FGTDevice in db.getAll():
+    	ID         = FGTDevice[0]
+    	STATUS     = FGTDevice[1]
+    	NAME	   = FGTDevice[2]
+    	SERIAL     = FGTDevice[3]
+    	CLIENT     = FGTDevice[4]
+    	IP         = FGTDevice[5]
+    	IP2        = FGTDevice[6]
+    	PORT	   = FGTDevice[7]
+    	USER       = FGTDevice[8]
+    	PASSWORD   = FGTDevice[9]
+    	FREQ_CHECK = FGTDevice[10]
 
-				if FGTInfo[0] != NAME:
-						print "UPDATE NAME"
-				if FGTInfo[1] != SERIAL:
-						print "UPDATE SERIAL"
+    	IPAddr = fgt.connFGT(IP, IP2, PORT)
+    	print IPAddr
+    	if IPAddr:
+    		outFGTInfo = fgt.runFGTCommand(USER, PASSWORD, IPAddr, PORT, "get system status")
+    		if "FortiOS" in outFGTInfo:
+    			FGTInfo = fgt.getFGTInfo(outFGTInfo)
+    			FGTName   = FGTInfo[0]
+    			FGTSerial = FGTInfo[1]
 
-				clientRepo = "%s/%s" % (defaultRepo, CLIENT)
-				if not os.path.exists(clientRepo):
-					os.makedirs(clientRepo)
-					uid = pwd.getpwnam(FTPUser).pw_uid
-					gid = grp.getgrnam(FTPUser).gr_gid
-					os.chown(clientRepo, uid, gid)
+    			if FGTInfo[0] != NAME:
+    					print "UPDATE NAME"
+    			if FGTInfo[1] != SERIAL:
+    					print "UPDATE SERIAL"
 
-				bkpFile = "%s/bkp_fgtconfig_%s.conf" % (CLIENT, FGTSerial)
+    			clientRepo = "%s/%s" % (defaultRepo, CLIENT)
+    			if not os.path.exists(clientRepo):
+    				os.makedirs(clientRepo)
+    				uid = pwd.getpwnam(FTPUser).pw_uid
+    				gid = grp.getgrnam(FTPUser).gr_gid
+    				os.chown(clientRepo, uid, gid)
 
-				strBkpCMD = 'exec backup full-config ftp %s %s %s %s' % (bkpFile, FTPHost, FTPUser, FTPPass)
-				print strBkpCMD
-				outFGTBkpInfo = fgt.runFGTCommand(USER, PASSWORD, IPAddr, PORT, strBkpCMD)
-				print outFGTBkpInfo
-				if "Send config file to ftp server OK" in outFGTBkpInfo:
-					print "Backup OK"
-					print "Starting to GIT"
-					repo.git.add(bkpFile)
-					CommitHash = ""
-					fcommit = repo.git.commit( m='Initial commit' )
-					regexGitHash = r"(.*)\ (\w+)\]\ (.*)"
-					if re.search(regexGitHash, fcommit):
-						strFind = re.search(regexGitHash, fcommit)
-						CommitHash = strFind.group(2)
-					print "Hash: %s" % (CommitHash)
-				elif "Return code 10" in outFGTBkpInfo:
-					print "Error: Login Failed"
+    			bkpFile = "%s/bkp_fgtconfig_%s.conf" % (CLIENT, FGTSerial)
+
+    			strBkpCMD = 'exec backup full-config ftp %s %s %s %s' % (bkpFile, FTPHost, FTPUser, FTPPass)
+    			print strBkpCMD
+    			outFGTBkpInfo = fgt.runFGTCommand(USER, PASSWORD, IPAddr, PORT, strBkpCMD)
+    			print outFGTBkpInfo
+    			if "Send config file to ftp server OK" in outFGTBkpInfo:
+    				print "Backup OK"
+    				print "Starting to GIT"
+    				repo.git.add(bkpFile)
+    				CommitHash = ""
+    				fcommit = repo.git.commit( m='Initial commit' )
+    				regexGitHash = r"(.*)\ (\w+)\]\ (.*)"
+    				if re.search(regexGitHash, fcommit):
+    					strFind = re.search(regexGitHash, fcommit)
+    					CommitHash = strFind.group(2)
+    				print "Hash: %s" % (CommitHash)
+    			elif "Return code 10" in outFGTBkpInfo:
+    				print "Error: Login Failed"
 
 if __name__ == "__main__":
-	main()
 
+    configfile = "config.yaml"
+    if sys.argv:
+        for x in range(1,len(sys.argv)):
+            if sys.argv[x] == "--configfile" or sys.argv[x] == "-c":
+                UserGroup = sys.argv[x + 1]
+
+            if sys.argv[x] == "--debug":
+                Debug = True
+
+            if sys.argv[x] == "--help" or sys.argv[x] == "-h":
+                print "--configfile or -c    [path/to/config/file]"
+
+    main(configfile)
